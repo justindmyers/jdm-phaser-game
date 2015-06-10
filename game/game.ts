6df2d8b1a133394465a69f16fe50a5ca3c3cf7d1
Justin Myers <justin.myers@responsivepath.com>
2015-06-10 16:21:31 -0500

    Added logo and correct path

--- game/game.ts
+++ game/game.ts
@@ -7,7 +7,7 @@ class SimpleGame {
     game: Phaser.Game;
 
     preload() {
-        this.game.load.image('logo', 'phaser2.png');
+        this.game.load.image('logo', '/game/images/Phaser-Logo-Small.png');
     }
 
     create() {
