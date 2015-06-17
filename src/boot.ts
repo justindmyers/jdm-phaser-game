c48776faa1570568869addc515b1c27c3df32b59
Justin Myers <justin.myers@responsivepath.com>
2015-06-17 00:11:57 -0400

    Added extra styling for UI. Removed auto-centering of game.

--- src/boot.ts
+++ src/boot.ts
@@ -10,16 +10,16 @@ module PhaserGame {
         }
 
         create() {
-            //  Unless you specifically need to support multitouch I would recommend setting this to 1
+            //Unless you specifically need to support multitouch I would recommend setting this to 1
             this.input.maxPointers = 1;
 
-            //  Phaser will automatically pause if the browser tab the game is in loses focus. You can disable that here:
+            //Phaser will automatically pause if the browser tab the game is in loses focus. You can disable that here:
             this.stage.disableVisibilityChange = true;
 
             if (this.game.device.desktop) {
-                //  If you have any desktop specific settings, they can go in here
+                //If you have any desktop specific settings, they can go in here
                 //this.stage.scale.pageAlignHorizontally = true;
-                this.game.scale.pageAlignHorizontally = true;
+                //this.game.scale.pageAlignHorizontally = true;
             }
             else {
                 //  Same goes for mobile settings.
