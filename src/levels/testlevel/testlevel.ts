37cd2efcb3182f7e377585e281d90ebddc1432d2
Justin Myers <justin.myers@responsivepath.com>
2015-06-27 17:17:12 -0500

    Added extra parallax layer

--- src/levels/testlevel/testlevel.ts
+++ src/levels/testlevel/testlevel.ts
@@ -6,20 +6,29 @@ module PhaserGame {
         player: Player;
         spawns: Object;
         background: Phaser.TileSprite;
+        background2: Phaser.TileSprite;
         maxCamera: number;
         backgroundCameraPosition: number;
+        backgroundCameraPosition2: number;
         
         preload() {
             // the key will be the filename of the map without the extension.
             this.game.load.pack('test', '/assets/tilemap-assets.json');
-            this.game.load.image('background', 'assets/background.gif');
+            this.game.load.image('background', 'assets/background.png');
+            this.game.load.image('background2', 'assets/background-2.png');
         }
         
         create() {
+            this.stage.backgroundColor = "#4ac6fe";
+
+            this.background2 = this.game.add.tileSprite(0, 0, 1920, 1600, 'background2'); //add the background            
             this.background = this.game.add.tileSprite(0, 0, 1920, 1600, 'background'); //add the background
+            
             this.background.fixedToCamera = true;
+            this.background2.fixedToCamera = true;
             
             this.background.tileScale.set(0.5, 0.5);
+            this.background2.tileScale.set(0.5, 0.5);
             
             this.map = this.game.add.tiledmap('test');
             var mapObjects = Lazy(this.map.objects),
@@ -39,6 +48,9 @@ module PhaserGame {
         update() {
            this.backgroundCameraPosition = (this.game.camera.y * .15) - (this.maxCamera * .15);
            this.background.tilePosition.set(this.game.camera.x * -0.25, -this.backgroundCameraPosition + (this.game.height * 2) - (this.background.height));
+           
+           this.backgroundCameraPosition2 = (this.game.camera.y * .15) - (this.maxCamera * .15);
+           this.background2.tilePosition.set(this.game.camera.x * -0.35, -this.backgroundCameraPosition + (this.game.height * 2) - (this.background.height));
             
             //only move camera if character is over 50%
             if (this.cursor.up.isDown)
