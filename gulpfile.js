c7c4ba7725a4aaeef6974e8ce479cb1099c01e25
Justin Myers <justin.myers@responsivepath.com>
2015-07-12 22:44:01 -0500

    Updated build process

--- gulpfile.js
+++ gulpfile.js
@@ -1,25 +1,45 @@
 var gulp = require('gulp'),
     typescript = require('gulp-typescript'),
     typescriptConfig = require('./tsconfig.json'),
-    tiledmapPack = require('gulp-phaser-tiled-pack');
+    tiledmapPack = require('gulp-phaser-tiled-pack'),
+    install = require("gulp-install"),
+    tsd = require('gulp-tsd');
 
 /*****
  * Assets Phaser packs task, creates phaser asset loader packs for tilemaps
  *****/
+gulp.task('default', ['build', 'tsd', 'pack']); 
+ 
 gulp.task('pack', function () {
     pack();
     copyLevels();
 });
 
 gulp.task('compile', function() {
-    compile();
+    return compile();
 });
+
+gulp.task('build', function() {
+    return build();
+});
+
+gulp.task('tsd', function(callback) {
+    return tsd({
+        command: 'reinstall',
+        config: './tsd.json'
+    }, callback);
+});
+
+function build() {
+    gulp.src(['./bower.json', './package.json'])
+        .pipe(install());    
+}
 
 function compile() {    
     var tsResult = gulp.src('src/**/*.ts')
         .pipe(typescript(typescriptConfig.compilerOptions));
         
-    return tsResult.js.pipe(gulp.dest(''));
+    tsResult.js.pipe(gulp.dest(''));
 }
 
 function pack() {
